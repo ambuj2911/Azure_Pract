06760ff3aa104df76158c731438b1c257ee1faee
ambuj2911 <guhey.ambuj@gmail.com>
2020-04-22 11:03:17 +0530

    First commit practice

--- test_deploy.py
+++ test_deploy.py
@@ -1,10 +1,12 @@
 
-from flask import Flask,render_template,request
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello"
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
